5bd686347dcd4b73ea5bdf2d3493cbb2f728e0e9
dashuncel <korovkinada@ya.ru>
2018-09-14 14:00:26 +0800

    'v1.0'

--- hw35_curr.py
+++ hw35_curr.py
@@ -1,13 +1,31 @@
 import osa
-import requests
+import datetime
 
-URI = "http://fx.currencysystem.com/webservices/CurrencyServer4.asmx"
+'''
+Вы собираетесь отправиться в путешествие и начинаете разрабатывать маршрут и выписывать цены на перелеты. 
+Даны цены на билеты в местных валютах (файл currencies.txt). Формат данных в файле:
 
-amt = []
+<откуда куда>: <стоимость билета> <код валюты>
+Пример:
+
+MOSCOW-LONDON: 120 EUR
+Посчитайте, сколько вы потратите на путешествие денег в рублях (без копеек, округлить в большую сторону).
+'''
+
+URI = "http://fx.currencysystem.com/webservices/CurrencyServer4.asmx?WSDL"
+
+def getRubAmount(fromCurr, amt):
+    client = osa.Client(URI)
+    resp = client.service.ConvertToNum(toCurrency="RUB", fromCurrency=fromCurr, amount=amt, rounding=True)
+    return resp
+
+amt = []# список сумм в рублях
+
 with open('currencies.txt', 'r') as f:
     for line in f:
         curr = line.split(' ')[2].strip()
-        amount = int(line.split(' ')[1].strip())
-        print(curr, amount)
+        amount = float(line.split(' ')[1].strip())
+        amt.append(getRubAmount(curr, amount))
 
+print(f'Сумма затрат на поездку по курсу на {datetime.datetime.now().strftime("%d-%m-%Y %H:%M")} = ', round(sum(amt)))
 
