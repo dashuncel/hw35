518bcac689f826ff57a856ce5bbce9f088e7beff
dashuncel <korovkinada@ya.ru>
2018-09-21 16:53:33 +0800

    v2.0

--- hw35_temp.py
+++ hw35_temp.py
@@ -1,46 +1,31 @@
 import osa
 import requests
 import statistics
+import xml.etree.ElementTree as ET
 
 '''
 Дано: семь значений температур по Фаренгейту в файле temps.txt. 
 Необходимо вывести среднюю за неделю арифметическую температуру по Цельсию.
 '''
-URI = 'https://www.w3schools.com/xml/tempconvert.asmx'
-
-# via osa
-def get_celsius(**params):
-    print(params)
-    if params['unit'] != "F":
-        return params['degree']
-
-    client = osa.Client(URI + '?WSDL')
-    resp = client.service.FahrenheitToCelsius(params['degree'])
-    #print(resp)
-    #return resp
-    return 1
+URI = 'https://www.w3schools.com/xml/tempconvert.asmx?WSDL'
 
 # via requests
 def get_celsius_request(**params):
     if params['unit'] != "F":
         return params['degree']
-
-    headers = {'Content-Type': 'text/xml; charset=utf-8'}
-    data = f'''
-            <?xml version="1.0" encoding="utf-8"?>
-            <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-            <soap:Body>
-                <FahrenheitToCelsius xmlns="https://www.w3schools.com/xml/">
-                    <Fahrenheit>{params['degree']}</Fahrenheit>
-                </FahrenheitToCelsius>
-            </soap:Body>
-            </soap:Envelope>
-            '''
-    #print(data)
+    headers = {'Content-Type': 'text/xml'}
+    data = f'''<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <FahrenheitToCelsius xmlns="https://www.w3schools.com/xml/">
+      <Fahrenheit>{params['degree']}</Fahrenheit>
+    </FahrenheitToCelsius>
+  </soap:Body>
+</soap:Envelope>
+'''
     resp = requests.post(URI, headers=headers, data=data)
-    #return resp
-    #print(resp)
-    return 1
+    tree = ET.fromstring(resp.content)
+    return(float(tree[0][0][0].text))
 
 
 def readFile(fileName='temps.txt'):
@@ -63,5 +48,5 @@ def readFile(fileName='temps.txt'):
 
 celsius = list(map(lambda f: get_celsius_request(**f), readFile()))
 print(celsius)
-print('Средняя температура за неделю:', statistics.mean(celsius))
+print('Средняя температура за неделю:', round(statistics.mean(celsius), 2))
 
